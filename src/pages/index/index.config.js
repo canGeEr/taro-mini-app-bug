d1415931d266c969a937c6852dff3e6a325955de
shepijcanwu <shepijcanwu@tencent.com>
2023-11-07 17:28:20 +0800

    feat: 主包页面未使用的原生小程序组件, 在分包使用会报错

--- src/pages/index/index.config.js
+++ src/pages/index/index.config.js
@@ -3,7 +3,7 @@ export default definePageConfig({
   usingComponents: {
     'custom-header': "./../page-one/components/header/index",
     'custom-source': "../../components/custom-source",
-    'bridge-parse': "../../components/bridge-parse"
+    // 'bridge-parse': "../../components/bridge-parse"
   },
   componentPlaceholder: {
     'custom-header': "view"
